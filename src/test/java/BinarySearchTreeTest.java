45931cd0708699459d0dd030b693491fdb1b0059
YuzheLin0310 <yuzhelin@my.yorku.ca>
2025-11-28 15:12:13 -0500

    Add unit tests for MyDoublyLinkedList

--- src/test/java/BinarySearchTreeTest.java
+++ src/test/java/BinarySearchTreeTest.java
@@ -1,1 +1,12 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class MyDoublyLinkedListTest {
+
+    private MyDoublyLinkedList<Integer> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new MyDoublyLinkedList<>();
+    }
